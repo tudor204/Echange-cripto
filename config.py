fde686c6f9a59639b42a109d627d7d2da01d21c2
Tudor <tudor204@hotmail.com>
2025-06-19 13:17:13 +0200

    últimos cambios

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 CRYPTO_LIST = [
     'BTC', 'ETH', 'BNB', 'USDT', 'ADA', 'SOL', 'XRP', 'DOT', 'LTC', 'DOGE',
     'AVAX', 'SHIB', 'MATIC', 'TRX', 'UNI', 'ATOM', 'LINK', 'XLM', 'BCH', 'VET', 'EUR'
@@ -11,3 +12,8 @@ ICONOS_CRIPTOS = {
     'ATOM': 'atom.svg', 'ETC': 'etc.svg', 'TRX': 'trx.svg', 'EUR': 'eur.svg'
 }
 
+API_BASE_URL = "https://rest.coinapi.io/v1/exchangerate"
+API_TIMEOUT = 5 # segundos
+
+COINAPI_KEY = os.getenv("COINAPI_KEY")
+ORIGIN_DATA="data/cripto.sqlite"
